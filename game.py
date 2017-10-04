e88d9ae9a09cf21a866f81c50114991d846f2521
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-03 18:01:07 -0700

    @wip Add a range option

--- game.py
+++ game.py
@@ -1,17 +1,17 @@
 import random
 
-def sillygame():
+def sillygame(low,high):
     """A number-guessing game."""
-    secret_number = random.randint(1,100)
+    secret_number = random.randint(low,high)
 
     guesses = 0
     guessed = False
-    too_low = 1
-    too_high = 100
+    too_low = low
+    too_high = high
 
     print "Shh... the secret number is %d" % secret_number
 
-    print "The starting boundary is between 1 and 100."
+    print "The starting boundary is between %d and %d." % (low,high)
 
     while guessed is False:
         number = raw_input("Guess a number:")
@@ -40,6 +40,12 @@ def sillygame():
             return guesses
         print "Your new boundary is %d and %d" % (too_low, too_high)
 
+def score_conversion(x, y, z):
+    #score, possiblities (number of ints in range)
+    #weighted score = score / possibilites
+    result = ((x/(z-y))*()
+    #return the conerted score
+
 
 name = raw_input("Hello, what's your name?")
 print "Hello %s!" % name
@@ -47,13 +53,22 @@ best_score = None
 while True:
     playwithme = raw_input("Do you want to play a silly guessing game? Y or N ").upper()
     if playwithme == "Y":
-        score = sillygame()
+        ask_bounds = raw_input("Do you want to set the bounds? Y or N").upper()
+        if ask_bounds == "N":
+            score = sillygame(0,100)
+        else:
+            low_n = int(raw_input("What is the lowest number? "))
+            high_n = int(raw_input("What is the highest number? "))
+            score = sillygame(low_n,high_n) #pass low and high
         if score == None:
             print "Try again."
-        elif score < best_score or best_score is None:
+        elif best_score is None:
             best_score = score
             print "Good job! You got the best score: %d" % best_score
-        else:
+        elif score_conversion(score, low_n, high_n) < best_score:
+            #analyze score in new func
+            
+            #print you win!
             print "Try again! The best score is still: %d" % best_score
     else:
         print "Peace out, homes."
